efd779e1ec98d125c8730e9cf75b5161912c4452
abishek-as <abeshek.as@gmail.com>
2023-11-17 02:17:18 +0530

    add: clerk auth

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="flex items-center justify-center h-full w-full">
+            {children}
+        </div>
+    );
+};
+
+export default ClerkLayout;
---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+    name: "TaskFlow",
+    description:
+        "Collaborate, manage projects, and reach new productivity peaks",
+};
