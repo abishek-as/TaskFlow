b9799170ab69dfc2a65e3bc75070d9b71adfb60d
abishek-as <abeshek.as@gmail.com>
2023-11-22 01:14:48 +0530

    add: board page

---
+++ actions/delete-board/types.ts
@@ -0,0 +1,7 @@
+import { ActionState } from "@/lib/create-safe-action";
+import { Board } from "@prisma/client";
+import { z } from "zod";
+import { DeleteBoard } from "./schema";
+
+export type InputType = z.infer<typeof DeleteBoard>;
+export type ReturnType = ActionState<InputType, Board>;
--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { auth } from "@clerk/nextjs";
+import { startCase } from "lodash";
+
 import { OrgControl } from "./_components/org-control";
+
+export async function generateMetadata() {
+    const { orgSlug } = auth();
+
+    return {
+        title: startCase(orgSlug || "organization"),
+    };
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
     return (
