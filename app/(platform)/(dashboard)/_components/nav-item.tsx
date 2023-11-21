997cee4856338cef4889ca516813c80dd80d017d
abishek-as <abeshek.as@gmail.com>
2023-11-21 13:56:10 +0530

    add: settings

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -10,6 +10,7 @@ import {
     AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 
 export type Organization = {
@@ -104,5 +105,16 @@ export const NavItem = ({
                 ))}
             </AccordionContent>
         </AccordionItem>
+    );
+};
+
+NavItem.Skeleton = function SkeletonNavItem() {
+    return (
+        <div className="flex items-center gap-x-2">
+            <div className="w-10 h-10 relative shrink-0">
+                <Skeleton className="h-full w-full absolute" />
+            </div>
+            <Skeleton className="h-10 w-full" />
+        </div>
     );
 };
