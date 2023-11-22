1764ed06263fa574a4b85d1918210f2963584503
abishek-as <abeshek.as@gmail.com>
2023-11-22 18:18:47 +0530

    add: drag and drop functionality

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Draggable } from "@hello-pangea/dnd";
 import { Card } from "@prisma/client";
 
 interface CardItemProps {
@@ -9,11 +10,18 @@ interface CardItemProps {
 
 export const CardItem = ({ data, index }: CardItemProps) => {
     return (
-        <div
-            role="button"
-            className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
-        >
-            {data.title}
-        </div>
+        <Draggable draggableId={data.id} index={index}>
+            {(provided) => (
+                <div
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                    role="button"
+                    className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+                >
+                    {data.title}
+                </div>
+            )}
+        </Draggable>
     );
 };
